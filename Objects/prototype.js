6615ed827027181006982ee33f3d4e728c964bdb
Kundan Singh <kundan.singh979@gmail.com>
2017-12-18 22:55:04 +0530

    Prototype explained

---
+++ Objects/prototype.js
@@ -0,0 +1,48 @@
+/**
+ * The prototype on a  function is NOT the prototype of THE function.
+ * 
+ * It's the protype of the Object created, if you are using the function
+ * as a function constructor.
+ */
+
+ function Person(firstname, lastname){
+	 console.log(this);
+	 this.firstname = firstname;
+	 this.lastname = lastname;
+ }
+
+ /**
+  * Every function that we create in javascript, gets this special prototype '.prototype'.
+  * It starts its life as an empty object.
+  * It's always there, you can add on it. e.g.,
+  */
+Person.prototype.getFullName = function(){
+	return this.firstname + ' ' + this.lastname;
+};
+
+/**
+ * The neat thing is that, we can add something to my prototype,
+ * on the fly, later, just like we did just now because the prototype
+ * chain is just looking at this object at the moment you try to
+ * access any one of their methods and properties.
+ */
+
+var john = Person('John', 'Doe');
+console.log(john.getFullName());
+
+var jane = Person('Jane', 'Doe');
+console.log(jane.getFullName());
+
+Person.prototype.getFormalFullName = function(){
+	return this.lastname + ', ' + this.firstname;
+}
+console.log(john.getFormalFullName());
+
+/**
+ * ========
+ *   NOTE:
+ * ========
+ * 
+ * From efficiency standpoint, it's better to put your methods
+ * on the prototype because they only need one copy to be used.
+ */
